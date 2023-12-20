1c4bafeef04e255771a00e07c2b88d8f176b1896
traore <abdoulaye.traore@nticagileconsuting.com>
2023-12-20 14:29:53 +0100

    first commit

---
+++ src/main/java/com/operantic/oterredesperancecqrses/OterredesperanceCqrsEsApplication.java
@@ -0,0 +1,13 @@
+package com.operantic.oterredesperancecqrses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OterredesperanceCqrsEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OterredesperanceCqrsEsApplication.class, args);
+	}
+
+}
